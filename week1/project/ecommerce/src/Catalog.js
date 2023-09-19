772b9e9a65fcf11d2e0535cf926cf4169471f08e
Audrey <keozon@gmail.com>
2023-09-19 15:19:36 +0200

    fixed selecting category

--- week1/project/ecommerce/src/Catalog.js
+++ week1/project/ecommerce/src/Catalog.js
@@ -1,39 +1,24 @@
-// Import React, { useState, useEffect } from 'react';
-import React, { useState, useEffect} from 'react';
-
-// Import categoriesData from an external file containing category data
+import React, { useState } from 'react';
 import categoriesData from './fake-data/all-categories.js';
-
-// Import the ProductsList component to display the list of products
 import ProductsList from './ProductsList';
 
-// Define the Catalog component, responsible for displaying product categories and filtered products, it receives props as an argument.
-
 const Catalog = (props) => {
-  //Within the Catalog component, we use the useState hook to initialize a piece of state named selectedCategory. This state variable will hold the currently selected product category. Initially, it's set to null because no category is selected by default.
-  let [selectedCategoryClean, setSelectedCategoryClean] = useState(null); // Add a state for cleaned category
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [showAllProducts, setShowAllProducts] = useState(true);
 
-  useEffect(() => {
-    // Update selectedCategoryClean whenever selectedCategory changes
-    const [selectedCategory, setSelectedCategory] = useState(null);
-    setSelectedCategoryClean(selectedCategory ? selectedCategory.replace(/.*:\s+/g, "") : selectedCategory);
-  }, [selectedCategory]);
-
-  // Clean the selected category name by removing additional characters
-    selectedCategoryClean = selectedCategory
-    ? selectedCategory.replace(/.*:\s+/g, '')
-    : null;
-
-    // Filter the props.products array (the complete list of products passed as a prop) to include only those products whose category property matches the cleaned selectedCategoryClean. If selectedCategoryClean is null, retain all products.
-  let filteredProducts = selectedCategoryClean
-    ? props.products.filter((product) => product.category === selectedCategoryClean)
-    : props.products;
-
-  // Define a function handleCategoryClick that takes a category as an argument. When a category is clicked, this function updates the selectedCategory state with the newly selected category.
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
+    setShowAllProducts(false);
   };
-  // Render the Catalog component. It displays a list of product categories based on categoriesData and applies a CSS class to the selected category. 
+
+  // Filter the props.products array when a category is selected
+  const filteredProducts = showAllProducts
+    ? props.products
+    : props.products.filter((product) => {
+        const cleanedCategory = selectedCategory ? selectedCategory.replace('FAKE: ', '') : '';
+        return product.category === cleanedCategory;
+      });
+
   return (
     <div className='App'>
       <div className='categories'>
@@ -41,7 +26,7 @@ const Catalog = (props) => {
           <div
             key={index}
             className={
-              category === selectedCategoryClean
+              selectedCategory === category
                 ? 'categories--item categories--item-selected'
                 : 'categories--item'
             }
@@ -56,5 +41,4 @@ const Catalog = (props) => {
   );
 };
 
-// Export the Catalog component as the default export
 export default Catalog;
