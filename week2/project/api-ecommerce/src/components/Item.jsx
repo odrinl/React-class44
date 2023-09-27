ea4b14ae0cc416cf7711371e30a0a13ffef35a81
Audrey <keozon@gmail.com>
2023-09-27 14:39:18 +0200

    delete rating in stars

--- week2/project/api-ecommerce/src/components/Item.jsx
+++ week2/project/api-ecommerce/src/components/Item.jsx
@@ -35,20 +35,6 @@ function Item({ item, setItem }) {
               <p className='card-footer'>
                 <small className='text-body-secondary'>{`Raiting: ${item.rating.rate} from ${item.rating.count}`}</small>
               </p>
-              <div class='height-100 container d-flex justify-content-center align-items-center'>
-                <div class='card p-3'>
-                  <div class='d-flex justify-content-between align-items-center'>
-                    <div class='ratings'>
-                      <i class='fa fa-star rating-color'></i>
-                      <i class='fa fa-star rating-color'></i>
-                      <i class='fa fa-star rating-color'></i>
-                      <i class='fa fa-star rating-color'></i>
-                      <i class='fa fa-star'></i>
-                    </div>
-                    <h5 class='review-count'>12 Reviews</h5>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </div>
